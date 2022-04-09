501925fc90fd918c24c17cba6ec47a133cd738f2
Leslie Cop <leslie.cop94@gmail.com>
2022-04-09 23:20:08 +0200

    final push

---
+++ js/main.js
@@ -0,0 +1,11 @@
+var burger_menu = document.querySelector('.burger');
+var menu = document.querySelector('.sider');
+var cross = document.querySelector('.cross');
+
+burger_menu.addEventListener('click', function () {
+	menu.classList.toggle('active');
+});
+
+cross.addEventListener('click', function () {
+	menu.classList.toggle('active');
+});
